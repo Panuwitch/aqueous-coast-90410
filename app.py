60adda56c563f5b321cda3eacbc24397262a5416
Panuwitch <panuwichy@gmail.com>
2021-11-02 21:59:20 +0700

    Make it better

--- app.py
+++ app.py
@@ -22,11 +22,6 @@ try:
 except InvalidArgument:
     raise
 
-# print("Query text:", response.query_result.query_text)
-# print("Detected intent:", response.query_result.intent.display_name)
-# print("Detected intent confidence:", response.query_result.intent_detection_confidence)
-# print("Fulfillment text:", response.query_result.fulfillment_text)
-
 
 @app.route('/')
 def Querytext():
